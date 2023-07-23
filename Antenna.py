424c93f7f2a19aafc0081e3aceb89adeb80adf2f
alexandropinion <86840853+alexandropinion@users.noreply.github.com>
2023-07-22 23:58:55 -0400

    Add abstract factory and singleton example

---
+++ Antenna.py
@@ -0,0 +1,101 @@
+"""
+This file is responsible for showing a tutorial example of the singleton design pattern.
+"""
+
+#: Imports
+import random
+
+#: Classes
+class Antenna():
+    """
+    Lets say we have a class, Antenna, that can handle the direction of a singlular antenna. Since we do not
+    have multiple antennas, and we only have one resource available, we will want to ensure that the class
+    controlling our one resource cannot be instantiated multiple times - this would cause a conflict with
+    our one resource, and could break something!
+    """
+ 
+    __public_inst: int = 1 # Keeps up with the instance for this class.
+
+ 
+    def __init__(self, enforce_singleton: bool):
+        """
+        Constructor that will enforce the class to only have one instance.
+        """
+        if enforce_singleton:
+            if Antenna.__public_inst != 1:
+                raise TypeError("Cannot instantiate class - class is desined to be a singleton class.")
+            else:
+                Antenna.public_inst = self
+        self.__x_direction: float = 0.0
+        self.__y_direction: float = 0.0
+        self.__z_direction: float = 0.0
+
+    def release_singleton(self) -> None:
+        self.__public_inst = None
+
+    @staticmethod
+    def current_inst() -> int:
+        """
+        This is a static method that allows a user to obtain the private handle used to ensure
+        that the instance is unique.
+        """
+        if Antenna.public_inst == 1:
+            Antenna()
+        return Antenna.public_inst
+    
+    def current_direction(self) -> tuple:
+        """
+        Returns the current direction of the antenna
+        """
+        return (self.__x_direction, self.__y_direction, self.__z_direction)
+    
+    def update_direction(self, x: float, y: float, z: float) -> None:
+        """
+        Updates the current direction of the antenna.
+        """
+        self.__x_direction = x
+        self.__y_direction = y
+        self.__z_direction = z
+
+def create_two_antennas(Antenna: Antenna) -> None:
+    """
+    Example function of a user trying to make two instances of the singleton class, Antenna.
+    We should expect to see an exception being thrown, if the Antenna class has been constructed properly
+    as a singleton.
+    """
+    try:
+        print(f"create_two_antennas(): Direction of the current antenna: {Antenna.current_direction()}")
+        Additional_Antenna = Antenna()
+    except Exception as e:
+        print(f"create_two_antennas(): Error occured: {e} <========")
+
+def new_antenna_direction(Antenna: Antenna) -> None:
+    """
+    We will update the resources inside the singleton and ensure that the updated resource(s) remain
+    for the singular allowable instance of the singleton class.
+    """
+    print(f"new_antenna_direction(): Current antenna direction: {Antenna.current_direction()}")
+    new_x: float = random.random()
+    new_y: float = random.random()
+    new_z: float = random.random()
+    print(f"new_antenna_direction(): The new desired x, y, and z direction for the antenna is: {new_x}, {new_y}, {new_z}")
+    Antenna.update_direction(x=new_x, y=new_y, z=new_z)
+    print(f"new_antenna_direction(): New antenna direction: {Antenna.current_direction()}")
+    print(f"new_antenna_direction(): instance handle: {antenna.current_inst()}")
+
+
+if __name__ == "__main__":
+    antenna = Antenna(enforce_singleton=True)
+    print(f"---------FIRST EXAMPLE (START) - ATTEMPT MULTIPLE INSTANTIATIONS ON SINGLETON--------")
+    create_two_antennas(Antenna=antenna)
+    print(f"--------------------------------FIRST EXAMPLE (END) ---------------------------------\n\n")
+
+    print(f"--------------SECOND EXAMPLE (START) - UPDATE RESOURCE IN SINGLETON CLASS------------")
+    new_antenna_direction(Antenna=Antenna(enforce_singleton=False))
+    print(f"--------------------------------SECOND EXAMPLE (END) --------------------------------\n\n")
+    
+    print(f"--------------THIRD EXAMPLE (START) - SHOW SAME OBJECT FOR TWO INSTANCES------------")
+    print(f"current antenna object reference: {antenna.current_inst()}")
+    new_antenna = Antenna(enforce_singleton=False)
+    print(f"new antenna object reference: {antenna.current_inst()}")
+    print(f"new antenna address: {new_antenna.current_inst()}")
---
+++ Cars.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+
+"""
+This file is responsible for showing a tutorial example of the abstract factory design pattern.
+"""
+
+#: Imports
+from abc import ABC, abstractmethod
+
+#: Classes
+class Cars(ABC): # ABSTRACT FACTORY
+    """
+    The primary interface that declares a collection of procedures for constructing abstract product objects is this one. 
+    It normally consists of a number of factory processes, each of which is in charge of producing a certain product type. 
+    An interface or an abstract class can both be used for the Abstract Factory.
+    """
+
+    @abstractmethod
+    def create_car(self, suv: bool):
+        pass
+
+
+class Sedan(ABC): # ABSTRACT PRODUCT
+    """
+    The Abstract Factory interface is implemented by Concrete Factory classes. 
+    Each concrete plant is in charge of producing a group of connected goods.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.name: str
+        self.type: str
+
+    # Interface
+    @abstractmethod
+    def create_car(self, suv: bool):
+        pass
+
+
+class SUV(ABC): # ABSTRACT PRODUCT
+    """
+    The Abstract Factory interface is implemented by Concrete Factory classes. 
+    Each concrete plant is in charge of producing a group of connected goods.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.name: str
+        self.type: str
+
+    @abstractmethod
+    def create_car(self, suv: bool):
+        pass
+
+
+class FordFusion(Sedan): # CONCRETE PRODUCT A1
+    """
+    The Abstract Product interface is implemented by Concrete Product classes. 
+    Each Concrete Factory is in charge of producing a certain range of Concrete Products. 
+    The real objects that the client programs will use are represented by these products.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.name: str
+        self.type: str
+
+    def create_car(self, suv: bool):
+        self.name: str = "Ford Fusion"
+        self.type: str = "Sedan"
+
+
+class HondaCivic(Sedan): # CONCRETE PRODUCT A2
+    """
+    The Abstract Product interface is implemented by Concrete Product classes. 
+    Each Concrete Factory is in charge of producing a certain range of Concrete Products. 
+    The real objects that the client programs will use are represented by these products.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.name: str
+        self.type: str
+
+    def create_car(self, suv: bool):
+        self.name: str = "Honda Civic"
+        self.type: str = "Sedan"
+
+
+class FordExplorer(SUV): # CONCRETE PRODUCT B1
+    """
+    The Abstract Product interface is implemented by Concrete Product classes. 
+    Each Concrete Factory is in charge of producing a certain range of Concrete Products. 
+    The real objects that the client programs will use are represented by these products.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.name: str
+        self.type: str
+
+    def create_car(self, suv: bool):
+        self.name: str = "Ford Explorer"
+        self.type: str = "SUV"
+
+
+class HondaPilot(SUV): # CONCRETE PRODUCT B2
+    """
+    The Abstract Product interface is implemented by Concrete Product classes. 
+    Each Concrete Factory is in charge of producing a certain range of Concrete Products. 
+    The real objects that the client programs will use are represented by these products.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.name: str
+        self.type: str
+
+    def create_car(self, suv: bool):
+        self.name: str = "Honda Pilot"
+        self.type: str = "SUV"
+
+
+
+
+class DomesticCar(Cars): # CONCRETE FACTORY 1
+    """
+    The Abstract Factory interface is implemented by Concrete Factory classes. 
+    Each concrete plant is in charge of producing a group of connected goods. 
+    """
+    
+    def create_car(self, suv: bool):
+        if suv:
+            return FordExplorer()
+        else:
+            return FordFusion()
+
+
+class InternationalCar(Cars): # CONCRETE FACTORY 2
+    """
+    The Abstract Factory interface is implemented by Concrete Factory classes. 
+    Each concrete plant is in charge of producing a group of connected goods.
+    """
+    def create_car(self, suv: bool):
+        if suv:
+            return HondaPilot()
+        else:
+            return HondaCivic()
+
+    
+
+def client(): 
+    """
+    The client code is in charge of building families of related objects using the Abstract Factory and Abstract Product interfaces. 
+    It is unaware of the specific product classes or the methods used in their production. 
+    Instead, it uses the objects' abstract interfaces to interact with them.
+    """
+    for factory in (DomesticCar(), InternationalCar()):
+        product1 = factory.create_car(suv=True)
+        product2 = factory.create_car(suv=False)
+        print(f"This is factory # {factory.__class__} <-----\nProduct A: {product1}\nProduct B: {product2}\n\n\n")
+
+
+if __name__ == "__main__":
+    client()
